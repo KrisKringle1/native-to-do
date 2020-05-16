b12c1d714cfd49dde0720b3053a98c3aebd050ce
Kris Ringler <kmringler@gmail.com>
2020-05-16 16:39:39 -0700

    added bootstrap but still struggling with form submission and error handling

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -3,11 +3,12 @@ import React, { useState } from 'react'
 const AddItem = () => {
   const [text, setText] = useState('');
   const onChange = (textValue) => setText(textValue)
+  const addItemToList = () => { }
 
   return (
     <div>
       <input type="text" onChange={onChange}></input>
-
+      <button onSubmit={() => addItemToList(text)}>Add Item</button>
     </div>
   )
 }
---
+++ src/components/PageTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function PageTitle(props) {
+  return (
+    <h1 className="mb-4">To Do List</h1>
+  );
+}
+
+export default PageTitle;
