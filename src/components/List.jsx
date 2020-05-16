63d66a3108836be24efc59d9a97163cd46e469ec
Kris Ringler <kmringler@gmail.com>
2020-05-16 00:07:16 -0700

    default set up with hello div:

---
+++ src/components/List.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ListItem from './ListItem'
+
+const List = () => {
+  return (
+    <div>
+      <form>
+        <ListItem />
+      </form>
+    </div>
+  )
+}
+
+export default List;
